7837035593f0f29fd35ac31badbfd9c2e8c6e94c
Yurytop <yakshin.mo@yandex.ru>
2022-08-19 14:01:07 +0300

    filter avalibal

--- src/db/seeders/20220817134727-drugs.js
+++ src/db/seeders/20220817134727-drugs.js
@@ -2,22 +2,22 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     const arrDrugs = [
       {
-        name: 'Аспирин', price: 25, img: 'img/drugs/aspirin.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Аспирин', price: 25, img: '/img/drugs/aspirin.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Антибубмин', price: 40, img: 'img/drugs/bubmin.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Антибубмин', price: 40, img: '/img/drugs/bubmin.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Актив. уголь', price: 7, img: 'img/drugs/coal.jpeg', avail: false, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Актив. уголь', price: 7, img: '/img/drugs/coal.jpeg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Непсихуй', price: 1588, img: 'img/drugs/psih.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Непсихуй', price: 1588, img: '/img/drugs/psih.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Факиншит', price: 126, img: 'img/drugs/shit.jpg', avail: true, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Факиншит', price: 126, img: '/img/drugs/shit.jpg', avail: true, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Выпивон', price: 91, img: 'img/drugs/vipivon.jpg', avail: true, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Выпивон', price: 91, img: '/img/drugs/vipivon.jpg', avail: true, createdAt: new Date(), updatedAt: new Date(),
       },
 
     ];
