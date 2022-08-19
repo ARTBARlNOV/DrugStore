152aa4d34b353b6410059c66e241acbd315dd353
ArtBar9981 <scorp9981@gmail.com>
2022-08-19 10:14:08 +0300

    Update

--- src/db/seeders/20220817134727-drugs.js
+++ src/db/seeders/20220817134727-drugs.js
@@ -8,16 +8,16 @@ module.exports = {
         name: 'Антибубмин', price: 40, img: 'img/drugs/bubmin.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Активированный уголь', price: 7, img: 'img/drugs/coal', avail: false, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Активированный уголь', price: 7, img: 'img/drugs/coal.jpeg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Непсихуй', price: 1588, img: 'img/drugs/psih', avail: false, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Непсихуй', price: 1588, img: 'img/drugs/psih.jpg', avail: false, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Факиншит', price: 126, img: 'img/drugs/shit', avail: true, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Факиншит', price: 126, img: 'img/drugs/shit.jpg', avail: true, createdAt: new Date(), updatedAt: new Date(),
       },
       {
-        name: 'Выпивон', price: 91, img: 'img/drugs/vipivon', avail: true, createdAt: new Date(), updatedAt: new Date(),
+        name: 'Выпивон', price: 91, img: 'img/drugs/vipivon.jpg', avail: true, createdAt: new Date(), updatedAt: new Date(),
       },
 
     ];
